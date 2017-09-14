217e5c3c1f17a0cb76bfc507c6ec41a1ff887915
Mihkel Haava <mhaava@users.noreply.github.com>
2017-09-14 11:35:13 +0300

    Lisatud täiendused php koodile

--- esimene.php
+++ esimene.php
@@ -1,3 +1,23 @@
+<?php
+	//uhsdiaudahhd
+	$myName = "Mihkel";
+	$myFamilyName = "Haava";
+	
+	//hindan päeva osa
+	$hourNow = date("H");
+	$partOfDay = "";
+	if ($hourNow < 8) {
+		$partOfDay = "Varajane hommik";		
+	}
+	if ($hourNow >= 8 and $hourNow < 16) {
+		$partOfDay = "Koolipäev";
+	}
+	if ($hourNow > 16) {
+		$partOfDay = "Vabaaeg";
+	}
+//	echo $partOfDay;
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -5,11 +25,14 @@
 		<title>Mihkel Haava</title>
 	</head>
 	<body>
-		<h1>Mihkel Haava</h1>
+		<h1><?php echo $myName ." " .$myFamilyName; ?></h1>
 		<p>See veebileht on loodud õppetöö raames ning ei sisalda mingisugust tõsiseltvõetavat sisu!</p>
 		<h4>Ma ei oska siia midagi kirjutada</h4>
 		<?php
-			echo "Algas php õppimine";
+			echo "<p>Algas php õppimine</p>";
+			echo "<p>Täna on ";
+			echo date("d.m.Y") .", kell oli lehe avamise hetkel " . date("H:i:s");
+			echo " Hetkel on</p>".$partOfDay;
 		?>
 	</body>
 </html>
