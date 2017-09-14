fbe90638d62ed0418c909ec5af3e98ada7cdde97
Mihkel Haava <mhaava@users.noreply.github.com>
2017-09-14 10:47:23 +0300

    Lisatud esimene rida php koodi

---
+++ esimene.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" ></meta>
+		<title>Mihkel Haava</title>
+	</head>
+	<body>
+		<h1>Mihkel Haava</h1>
+		<p>See veebileht on loodud õppetöö raames ning ei sisalda mingisugust tõsiseltvõetavat sisu!</p>
+		<h4>Ma ei oska siia midagi kirjutada</h4>
+		<?php
+			echo "Algas php õppimine";
+		?>
+	</body>
+</html>
